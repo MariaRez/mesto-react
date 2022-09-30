aa4e516709b400afa178c9e1edffee817afc9c91
MariaRez <maria.v.reznik@yandex.ru>
2022-09-30 19:45:13 +0400

    Попапы создаются через соотвествующий компонент

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,153 +3,153 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import PopupWithImage from "../PopupWithImage/PopupWithImage";
+import PopupWithForm from "../PopupWithForm/PopupWithForm";
 
 function App() {
   return (
     <div className="body">
-          <div className="page">
-          <Header />
-          <Main />
-          <Footer />
-      <div className="popup popup_place_avatar">
-        <div className="popup__container">
-          <h3 className="popup__title">Обновить аватар</h3>
-          <form name="form_avatar" className="form form_place_avatar" noValidate>
-            <input
-              type="url"
-              placeholder="Ссылка на картинку"
-              className="popup__field popup__field_type_avatarlink form__input"
-              name="avatar"
-              id="avatar"
-              required
-            />
-            <span id="avatar-error" className="error form__input-error"></span>
-            <button className="popup__button" type="submit">
-              Сохранить
-            </button>
-            <button
-              aria-label="Close"
-              className="popup__close-button popup__close-button_place_profile"
-              type="button"
-            ></button>
-          </form>
-        </div>
-      </div>
-      <div className="popup popup_place_profile">
-        <div className="popup__container">
-          <h3 className="popup__title">Редактировать профиль</h3>
-          <form
-            name="form_profile"
-            className="form form_place_profile"
-            noValidate
-          >
-            <input
-              type="text"
-              placeholder="Имя"
-              className="popup__field popup__field_type_name form__input"
-              name="name"
-              id="name"
-              required
-              minLength="2"
-              maxLength="40"
-            />
-            <span id="name-error" className="error form__input-error"></span>
-            <input
-              type="text"
-              placeholder="О себе"
-              className="popup__field popup__field_type_description form__input"
-              name="about"
-              id="about"
-              required
-              minLength="2"
-              maxLength="200"
-            />
-            <span id="about-error" className="error form__input-error"></span>
-            <button className="popup__button" type="submit">
-              Сохранить
-            </button>
-            <button
-              aria-label="Close"
-              className="popup__close-button popup__close-button_place_profile"
-              type="button"
-            ></button>
-          </form>
-        </div>
-      </div>
-      <div className="popup popup_place_card">
-        <div className="popup__container">
-          <h3 className="popup__title">Новое место</h3>
-          <form name="form_card" className="form form_place_card" noValidate>
-            <input
-              type="text"
-              placeholder="Название"
-              className="popup__field popup__field_type_placename form__input"
-              name="name"
-              id="name-card"
-              required
-              minLength="2"
-              maxLength="30"
-            />
-            <span
-              id="name-card-error"
-              className="error form__input-error"
-            ></span>
-            <input
-              type="url"
-              placeholder="Ссылка на картинку"
-              className="popup__field popup__field_type_placelink form__input"
-              name="link"
-              id="link"
-              required
-            />
-            <span id="link-error" className="error form__input-error"></span>
-            <button
-              className="popup__button popup__button_place_card"
-              type="submit"
+      <div className="page">
+        <Header />
+        <Main />
+        <Footer />
+        <PopupWithForm
+          name="avatar"
+          title="Обновить аватар"
+          children={
+            <>
+              <input
+                type="url"
+                placeholder="Ссылка на картинку"
+                className="popup__field popup__field_type_avatarlink form__input"
+                name="avatar"
+                id="avatar"
+                required
+              />
+              <span
+                id="avatar-error"
+                className="error form__input-error"
+              ></span>
+              <button className="popup__button" type="submit">
+                Сохранить
+              </button>
+            </>
+          }
+        />
+        <PopupWithForm
+          name="profile"
+          title="Редактировать профиль"
+          children={
+            <>
+              <input
+                type="text"
+                placeholder="Имя"
+                className="popup__field popup__field_type_name form__input"
+                name="name"
+                id="name"
+                required
+                minLength="2"
+                maxLength="40"
+              />
+              <span id="name-error" className="error form__input-error"></span>
+              <input
+                type="text"
+                placeholder="О себе"
+                className="popup__field popup__field_type_description form__input"
+                name="about"
+                id="about"
+                required
+                minLength="2"
+                maxLength="200"
+              />
+              <span id="about-error" className="error form__input-error"></span>
+              <button className="popup__button" type="submit">
+                Сохранить
+              </button>
+            </>
+          }
+        />
+        <PopupWithForm
+          name="card"
+          title="Новое место"
+          children={
+            <>
+              <input
+                type="text"
+                placeholder="Название"
+                className="popup__field popup__field_type_placename form__input"
+                name="name"
+                id="name-card"
+                required
+                minLength="2"
+                maxLength="30"
+              />
+              <span
+                id="name-card-error"
+                className="error form__input-error"
+              ></span>
+              <input
+                type="url"
+                placeholder="Ссылка на картинку"
+                className="popup__field popup__field_type_placelink form__input"
+                name="link"
+                id="link"
+                required
+              />
+              <span id="link-error" className="error form__input-error"></span>
+              <button
+                className="popup__button popup__button_place_card"
+                type="submit"
+              >
+                Создать
+              </button>
+            </>
+          }
+        />
+        <PopupWithImage />
+        <div className="popup popup_place_delete">
+          <div className="popup__delete-container">
+            <h3 className="popup__small-title">Вы уверены?</h3>
+            <form
+              name="formDelete"
+              className="form form_place_delete"
+              noValidate
             >
-              Создать
-            </button>
-            <button
-              aria-label="Close"
-              className="popup__close-button popup__close-button_place_card"
-              type="button"
-            ></button>
-          </form>
+              <button
+                className="popup__button popup__button_place_delete"
+                type="submit"
+              >
+                Да
+              </button>
+              <button
+                aria-label="Close"
+                className="popup__close-button popup__close-button_place_image"
+                type="button"
+              ></button>
+            </form>
+          </div>
         </div>
       </div>
-      <PopupWithImage />
-      <div className="popup popup_place_delete">
-        <div className="popup__delete-container">
-          <h3 className="popup__small-title">Вы уверены?</h3>
-          <form name="formDelete" className="form form_place_delete" noValidate>
-            <button
-              className="popup__button popup__button_place_delete"
-              type="submit"
-            >
-              Да
-            </button>
-            <button
-              aria-label="Close"
-              className="popup__close-button popup__close-button_place_image"
-              type="button"
-            ></button>
-          </form>
+      <template className="elements-template">
+        <div className="element">
+          <img className="element__image" />
+          <button
+            aria-label="Delete card"
+            className="element__trash"
+            type="button"
+          ></button>
+          <div className="element__rectangle">
+            <h3 className="element__text"></h3>
+            <div className="element__like-zone">
+              <button
+                aria-label="Like card"
+                className="element__like"
+                type="button"
+              ></button>
+              <p className="element__like-counter">1</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    <template className="elements-template">
-    <div className="element">
-        <img className="element__image"/>
-        <button aria-label="Delete card" className="element__trash" type="button"></button>
-        <div className="element__rectangle">
-         <h3 className="element__text"></h3>
-         <div className="element__like-zone">
-          <button aria-label="Like card" className="element__like" type="button"></button>
-          <p className="element__like-counter">1</p>
-         </div>
-        </div>
-    </div>
-  </template>
+      </template>
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,52 +1,50 @@
-import React from 'react';
-import './Main.css';
+import React from "react";
+import "./Main.css";
 import avatar from "../../images/image.jpg";
 
-function Main () {
+function Main() {
   function handleEditAvatarClick() {
     console.log("Открыли попап для изменения аватара");
-    document.querySelector('.popup_place_avatar').classList.add('popup_opened');
+    document.querySelector(".popup_place_avatar").classList.add("popup_opened");
   }
   function handleEditProfileClick() {
     console.log("Открыли попап для изменения профиля");
-    document.querySelector('.popup_place_profile').classList.add('popup_opened');
+    document
+      .querySelector(".popup_place_profile")
+      .classList.add("popup_opened");
   }
   function handleAddPlaceClick() {
     console.log("Открыли попап для создании новой карточки");
-    document.querySelector('.popup_place_card').classList.add('popup_opened');
+    document.querySelector(".popup_place_card").classList.add("popup_opened");
   }
-    return (
-        <main>
-        <section className="profile">
-          <div className="profile__avatar" onClick={handleEditAvatarClick}>
-            <img
-              className="profile__avatar-image"
-              src={avatar}
-              alt="Аватарка"
-            />
-          </div>
-          <div className="profile__profile-info">
-            <div className="profile__name-button">
-              <h1 className="profile__name">Жак-Ив Кусто</h1>
-              <button
+  return (
+    <main>
+      <section className="profile">
+        <div className="profile__avatar" onClick={handleEditAvatarClick}>
+          <img className="profile__avatar-image" src={avatar} alt="Аватарка" />
+        </div>
+        <div className="profile__profile-info">
+          <div className="profile__name-button">
+            <h1 className="profile__name">Жак-Ив Кусто</h1>
+            <button
               onClick={handleEditProfileClick}
-                aria-label="Edit Profile"
-                className="profile__edit-button"
-                type="button"
-              ></button>
-            </div>
-            <p className="profile__description">Исследователь океана</p>
+              aria-label="Edit Profile"
+              className="profile__edit-button"
+              type="button"
+            ></button>
           </div>
-          <button
+          <p className="profile__description">Исследователь океана</p>
+        </div>
+        <button
           onClick={handleAddPlaceClick}
-            aria-label="Add card"
-            className="profile__add-button"
-            type="button"
-          ></button>
-        </section>
-        <section className="elements"></section>
-      </main>
-    );
+          aria-label="Add card"
+          className="profile__add-button"
+          type="button"
+        ></button>
+      </section>
+      <section className="elements"></section>
+    </main>
+  );
 }
 
 export default Main;
